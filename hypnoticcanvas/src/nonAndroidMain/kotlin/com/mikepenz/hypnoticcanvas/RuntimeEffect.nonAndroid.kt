a057e0505c994f14ff5cccc07f529452a44d27d3
Mike Penz <opensource@mikepenz.dev>
2024-03-10 19:24:26 +0100

    - improve handling for Android specifically for devices not supporting RuntimeEffects yet. - expose if shader is supported on the specific device - add link to repository to sample app

--- hypnoticcanvas/src/nonAndroidMain/kotlin/com/mikepenz/hypnoticcanvas/RuntimeEffect.nonAndroid.kt
+++ hypnoticcanvas/src/nonAndroidMain/kotlin/com/mikepenz/hypnoticcanvas/RuntimeEffect.nonAndroid.kt
@@ -1,5 +1,6 @@
 package com.mikepenz.hypnoticcanvas
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.ShaderBrush
 import com.mikepenz.hypnoticcanvas.shaders.Shader
 import org.jetbrains.skia.RuntimeShaderBuilder
@@ -9,12 +10,16 @@ class NonAndroidRuntimeEffect(shader: Shader) : RuntimeEffect {
     private val compositeRuntimeEffect = org.jetbrains.skia.RuntimeEffect.makeForShader(shader.sksl)
     private val compositeShaderBuilder = RuntimeShaderBuilder(compositeRuntimeEffect)
 
+    override val supported: Boolean = true
+    override var ready: Boolean = false
+
     override fun updateUniforms(time: Float, width: Float, height: Float) {
         compositeShaderBuilder.uniform("uResolution", width, height, width / height)
         compositeShaderBuilder.uniform("uTime", time)
+        ready = width > 0 && height > 0
     }
 
-    override fun build(): ShaderBrush {
+    override fun build(): Brush {
         return ShaderBrush(compositeShaderBuilder.makeShader())
     }
 }
