ca2d6a9b09177955340912dfa55464a7abb0cd54
Mike Penz <opensource@mikepenz.dev>
2024-12-26 10:09:35 +0100

    - upgrade dependencies to major new releases   - kotlin 2.1.0   - compose-mp 1.7.3   - gradle 8.12   - coil 3.0.4   - haze 1.1.1   - ...

--- gradle/build-logic/convention/src/main/kotlin/com/mikepenz/gradle/KotlinMultiplatformConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/com/mikepenz/gradle/KotlinMultiplatformConventionPlugin.kt
@@ -4,9 +4,9 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 class KotlinMultiplatformConventionPlugin : Plugin<Project> {
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
     override fun apply(target: Project) = with(target) {
         with(pluginManager) {
             apply("org.jetbrains.kotlin.multiplatform")
@@ -26,7 +26,6 @@ class KotlinMultiplatformConventionPlugin : Plugin<Project> {
             iosArm64()
             iosSimulatorArm64()
 
-            @OptIn(ExperimentalWasmDsl::class)
             wasmJs {
                 browser()
             }
